963a6cc16558830f4a9590aaa751326fa21ddf72
brino <catbrino@gmail.com>
2022-03-04 14:26:28 +0700

    update

--- Tugas Perulangan WHILE.py
+++ Tugas Perulangan WHILE.py
@@ -6,7 +6,7 @@ jumlah_buku = 10
 print("Andi ayo belajar, dibaca semua bukunya")
 
 jumlah_buku_dibaca = 0
-print(f"Buku ke {jumlah_buku_dibaca} sudah dibaca")
+print(f"Jumlah buku yang sudah dibaca {jumlah_buku_dibaca}")
 
 while jumlah_buku_dibaca < jumlah_buku:
     jumlah_buku_dibaca = jumlah_buku_dibaca + 1
