7815239fc04a458b57b483e9b012063689446511
brino <catbrino@gmail.com>
2022-03-04 13:55:36 +0700

    update

---
+++ Tugas Perulangan WHILE.py
@@ -0,0 +1,12 @@
+"""
+Tugas perulangan membaca buku
+"""
+
+jumlah_buku = 100
+print("Andi ayo belajar, dibaca semua bukunya")
+jumlah_buku_dibaca = 0
+
+for jumlah_buku_dibaca in range (1,jumlah_buku + 1):
+    print(f"Buku ke {jumlah_buku_dibaca} sudah dibaca")
+
+print(f"buku yang sudah terbaca sebanya {jumlah_buku_dibaca} buku")
