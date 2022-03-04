4e264035c42fc4a9fafacf222d4cdac8eaf32aa9
brino <catbrino@gmail.com>
2022-03-04 13:49:05 +0700

    update

--- Tugas Perulangan FOR.py
+++ Tugas Perulangan FOR.py
@@ -2,11 +2,11 @@
 Tugas perulangan membaca buku
 """
 
-Jumlah_buku = 10
+jumlah_buku = 100
 print("Andi ayo belajar, dibaca semua bukunya")
 jumlah_buku_dibaca = 0
 
-for jumlah_buku_dibaca in range (1,11):
+for jumlah_buku_dibaca in range (1,jumlah_buku + 1):
     print(f"Buku ke {jumlah_buku_dibaca} sudah dibaca")
 
 print(f"buku yang sudah terbaca sebanya {jumlah_buku_dibaca} buku")
