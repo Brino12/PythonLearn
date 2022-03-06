ceb8e3f51aaa4f3409cd7f09a373370615f1a672
brino <catbrino@gmail.com>
2022-03-06 12:48:29 +0700

    update

--- Modularisasi.py
+++ Modularisasi.py
@@ -7,26 +7,7 @@
 tidak berpotensi TSUNAMI
 """
 
-def ekstraksi_data():
-
-    hasil = dict()
-    hasil['tanggal'] = '06 Maret 2022'
-    hasil['waktu'] = '05:40:28 WIB'
-    hasil['magnitudo'] = '5.0'
-    hasil['kedalaman'] = '43 km'
-    hasil['lokasi'] = {'LU': 3.55, 'BT': 126.10}
-    hasil['potensi gempa'] = 'Tidak Berpotensi Tsunami'
-
-    return hasil
-
-def tampilkan_data(result):
-    print('Gempa Terakhir berdasarkan BMKG')
-    print(f"tanggal {result['tanggal']}")
-    print(f"waktu {result['waktu']}")
-    print(f"magnitudo {result['magnitudo']}")
-    print(f"kedalaman {result['kedalaman']}")
-    print(f"lokasi: LS={result['lokasi']['LS']}, BT={result['lokasi']['BT']}")
-    print(f"potensi gempa {result['potensi gempa']}")
+from deteksi_gempa import ekstraksi_data, tampilkan_data
 
 if __name__ == '__main__':
     print('Aplikasi Utama')
