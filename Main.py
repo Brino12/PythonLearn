39b3a1098c49db041b048f1301c1e2a6f566ef96
brino <catbrino@gmail.com>
2022-03-07 15:53:49 +0700

    update

--- Main.py
+++ Main.py
@@ -1,40 +1,7 @@
 """
 Deteksi gempa
 """
-
-def  ekstraksi_data():
-    """
-    Tanggal: 07 Maret 2022,
-    Waktu: 10:47:23 WIB
-    Magnitudo: 4.3
-    Kedalaman: 10 km
-    Lokasi: LS = 7.98  BT = 122.47
-    Pusat Gempa: Pusat gempa berada di laut 66 km BaratLaut Larantukan
-    Keterangan: Dirasakan (Skala MMI): I-II Larantuka, I-II Lembata
-    :return:
-    """
-    hasil = dict()
-    hasil['tanggal'] = '07 Maret 2022'
-    hasil['waktu'] = '10:47:23 WIB'
-    hasil['magnitudo'] = '4.3'
-    hasil['kedalaman'] = '10 km'
-    hasil['lokasi'] = {'ls': 7.98, 'bt': 122.47}
-    hasil['pusat'] = 'Pusat gempa berada di laut 66 km BaratLaut Larantukan'
-    hasil['keterangan'] = 'Dirasakan (Skala MMI): I-II Larantuka, I-II Lembata'
-    return hasil
-
-
-
-def tampilkan_data(result):
-    print('Gempa Terakhir Berdasarkan BMKG')
-    print(f"Tanggal {result['tanggal']}")
-    print(f"Waktu {result['waktu']}")
-    print(f"Magnitudo {result['magnitudo']}")
-    print(f"Kedalaman {result['kedalaman']}")
-    print(f"Lokasi: LS = {result['lokasi']['ls']}, BT = {result['lokasi']['bt']}")
-    print(f"Pusat {result['pusat']}")
-    print(f"Keterangan {result['keterangan']}")
-
+from deteksi_gempa import ekstraksi_data, tampilkan_data
 
 if __name__ == '__main__':
     print('Aplikasi Utama')
--- deteksi_gempa/__init__.py
+++ deteksi_gempa/__init__.py
@@ -1,44 +1,66 @@
 import requests
 from bs4 import BeautifulSoup
 
-
 def ekstraksi_data():
     try:
         content = requests.get('https://bmkg.go.id')
     except Exception:
         return None
+    if content.status_code == 200:
+        soup = BeautifulSoup(content.text, 'html.parser')
 
-        # if content.status_code == 200:
-        soup = BeautifulSoup('<p>Some<b>bad<i>HTML')
-        print(soup.prettify())
+        tarik = soup.find('span', {'class': 'waktu'})
+        tarik = tarik.text.split(', ')
+        tanggal = tarik[0]
+        waktu = tarik[1]
+
+        tekan = soup.find('div', {'class': 'col-md-6 col-xs-6 gempabumi-detail no-padding'})
+        tekan = tekan.findChildren('li')
+        i = 0
+        magnitudo = None
+        ls = None
+        bt = None
+        kedalaman = None
+        lokasi = None
+        dirasakan = None
+
+        for res in tekan:
+            if i == 1:
+                magnitudo = res.text
+            elif i == 2:
+                kedalaman = res.text
+            elif i == 3:
+                koordinat = res.text.split(' - ')
+                ls = koordinat[0]
+                bt = koordinat[1]
+            elif i == 4:
+                lokasi = res.text
+            elif i == 5:
+                dirasakan = res.text
+            i = i + 1
 
         hasil = dict()
-        hasil['tanggal'] = '06 Maret 2022'
-        hasil['waktu'] = '05:40:28 WIB'
-        hasil['magnitudo'] = '5.0'
-        hasil['kedalaman'] = '43 km'
-        hasil['lokasi'] = {'LU': 3.55, 'BT': 126.10}
-        hasil['potensi'] = 'Tidak Berpotensi Tsunami'
+        hasil['tanggal'] = tanggal
+        hasil['waktu'] = waktu
+        hasil['magnitudo'] = magnitudo
+        hasil['kedalaman'] = kedalaman
+        hasil['koordinat'] = {'ls': ls, 'bt': bt}
+        hasil['lokasi'] = lokasi
+        hasil['dirasakan'] = dirasakan
         return hasil
     else:
         return None
 
-
 def tampilkan_data(result):
-    # if result is None:
-    # print('Tidak bisa menemukan data gempa terkini')
-    # return
+    if result is None:
+        print('Tidak bisa menemukan data gempa terkini')
+        return
+    print('Gempa Terakhir Berdasarkan BMKG')
+    print(f"Tanggal {result['tanggal']}")
+    print(f"Waktu {result['waktu']}")
+    print(f"Magnitudo {result['magnitudo']}")
+    print(f"Kedalaman {result['kedalaman']}")
+    print(f"Koordinat: LS = {result['koordinat']['ls']}, BT = {result['koordinat']['bt']}")
+    print(f"Lokasi {result['lokasi']}")
+    print(f"Dirasakan {result['dirasakan']}")
 
-    print('Gempa Terakhir berdasarkan BMKG')
-    print(f"tanggal {result['tanggal']}")
-    print(f"waktu {result['waktu']}")
-    print(f"magnitudo {result['magnitudo']}")
-    print(f"kedalaman {result['kedalaman']}")
-    print(f"lokasi: LS={result['lokasi']['LS']}, BT={result['lokasi']['BT']}")
-    print(f"potensi {result['potensi']}")
-
-
-if __name__ != '__main__':
-    pass
-else:
-    print('Ini adalaha package deteksi gempa')
