574e6c4793d0a62e256e7ea0ddb4c5908f893741
brino <catbrino@gmail.com>
2022-03-07 13:15:30 +0700

    update

--- deteksi_gempa/__init__.py
+++ deteksi_gempa/__init__.py
@@ -8,35 +8,14 @@ def ekstraksi_data():
     except Exception:
         return None
 
-    if content.status_code == 200:
-        soup = BeautifulSoup(content.text, 'html.parser')
-
-        result = soup.find('span', {'class': 'waktu'})
-        result = result.text.split(', ')
-        tanggal = result[0]
-        waktu = result[1]
-
-        result = soup.find('div', {'class': 'col-md-6 col-xs-6 gempabumi-detail no-padding'})
-        result = result.findChildren('Li')
-        i = 0
-        magnitudo = None
-        ls = None
-        bt = None
-
-        for res in result:
-            print(i, res)
-            if i = 1:
-                magnitudo = res.text
-            elif i == 2:
-                lokasi =
-
-
-
+    #if content.status_code == 200:
+        soup = BeautifulSoup('<p>Some<b>bad<i>HTML')
+        print(soup.prettify())
 
         hasil = dict()
-        hasil['tanggal'] = tanggal.text #'06 Maret 2022'
-        hasil['waktu'] = waktu #'05:40:28 WIB'
-        hasil['magnitudo'] = magnitudo #'5.0'
+        hasil['tanggal'] = '06 Maret 2022'
+        hasil['waktu'] = '05:40:28 WIB'
+        hasil['magnitudo'] = '5.0'
         hasil['kedalaman'] = '43 km'
         hasil['lokasi'] = {'LU': 3.55, 'BT': 126.10}
         hasil['potensi gempa'] = 'Tidak Berpotensi Tsunami'
@@ -58,4 +37,5 @@ def tampilkan_data(result):
         print(f"lokasi: LS={result['lokasi']['LS']}, BT={result['lokasi']['BT']}")
         print(f"potensi gempa {result['potensi gempa']}")
 
-print('Ini adalah package gempa terkini')
+if __name__ == '__main__':
+    print('Ini adalaha package deteksi gempa')
